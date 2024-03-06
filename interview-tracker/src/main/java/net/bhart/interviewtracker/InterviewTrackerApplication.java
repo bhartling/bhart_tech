d4958daa2165c75f01e939e644c078be589d56d8
bhartling <bryan.hartling@gmail.com>
2024-03-06 13:26:26 -0500

    Sets up the frontend

--- interview-tracker/src/main/java/net/bhart/interviewtracker/InterviewTrackerApplication.java
+++ interview-tracker/src/main/java/net/bhart/interviewtracker/InterviewTrackerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class InterviewTrackerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(InterviewTrackerApplication.class, args);
-	}
+    public static void main(String[] args) {
+	    SpringApplication.run(InterviewTrackerApplication.class, args);
+    }
 
 }
