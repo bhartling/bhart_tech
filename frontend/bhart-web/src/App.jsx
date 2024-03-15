62c819e408db7df8ffe312f224d7d650584bd51f
bhartling <bryan.hartling@gmail.com>
2024-03-15 15:11:41 -0400

    Renamed directories and removed broken frontend app

--- frontend/bhart-web/src/App.jsx
+++ frontend/bhart-web/src/App.jsx
@@ -1,12 +1,13 @@
+import { useState } from 'react'
 import './App.css'
-import HelloWorld from './HelloWorld'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <>
-    <HelloWorld/>
-    </>
+    <div className="container">
+      <h1>Hey You Guys!</h1>
+    </div>
   )
 }
 
--- frontend/bhart-web/src/HelloWorld.jsx
+++
@@ -1,5 +0,0 @@
-function HelloWorld(){
-    return <h1 className="text-center">Hey you guys!</h1>
-}
-
-export default HelloWorld
